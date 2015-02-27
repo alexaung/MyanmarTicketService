073b37e0e5d28b342eb5ab95deb8e10ff662db7d
alexaung <aungmo@gmail.com>
2015-02-27 17:53:51 +0800

    Added Angular Form

---
+++ myanmarticket/App/Controllers/busController.js
@@ -0,0 +1,9 @@
+﻿busApp.controller('busController', function ($scope) {
+    // we will store all of our form data in this object
+    $scope.formData = {};
+
+    // function to process the form
+    $scope.processForm = function () {
+        alert('awesome!');
+    };
+});
---
+++ myanmarticket/App/app.js
@@ -0,0 +1,37 @@
+﻿var busApp = angular.module('BusTicketApp', ['ngAnimate', 'ui.router']);
+
+busApp.config(function ($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        // route to show our basic form (/form)
+        .state('bus', {
+            url: '/bus',
+            templateUrl: '/app/views/bus/bus.html',
+            controller: 'busController'
+        })
+        
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('bus.search', {
+            url: '/search',
+            templateUrl: '/app/views/bus/bus-search.html'
+        })
+        
+        // url will be /form/interests
+        .state('bus.result', {
+            url: '/result',
+            templateUrl: '/app/views/bus/bus-result.html'
+        })
+        
+        // url will be /form/payment
+        .state('bus.payment', {
+            url: '/payment',
+            templateUrl: '/app/views/bus/bus-payment.html'
+        });
+       
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/bus/search');
+})
